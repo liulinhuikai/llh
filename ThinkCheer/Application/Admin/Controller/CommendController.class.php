12606cf8b36b05c31198a5cb8f88a49091503710
wuyiwen0 <1317281881@qq.com>
2018-04-01 12:18:13 +0800

    后台添加推荐位功能完成

--- ThinkCheer/Application/Admin/Controller/CommendController.class.php
+++ ThinkCheer/Application/Admin/Controller/CommendController.class.php
@@ -24,4 +24,37 @@ class CommendController extends Controller {
       $this->assign("commend", $commend);
       $this->display();
     }
+    public function add(){
+    	$this->display();
+    }
+    //WYW同时上传音乐、图片文件，设置两个上传类
+    public function doAdd(){
+		$uploadimg = new \Think\Upload();// 实例化图片上传类
+
+	    $uploadimg->maxSize   =     3145728 ;// 设置附件上传大小
+	    $uploadimg->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+	    $uploadimg->rootPath  =     THINK_PATH; // 设置附件上传根目录
+	    $uploadimg->savePath  =     '../Public/uploads/'; // 设置附件上传（子）目录
+	    // 上传文件 
+	    $infoimg   =   $uploadimg->upload();
+
+	    $uploadmusic = new \Think\Upload();// 实例化音乐上传类
+
+	    $uploadmusic->maxSize   =     5*1024*1024 ;// 设置附件上传大小
+	    $uploadmusic->exts      =     array('mp3');// 设置附件上传类型
+	    $uploadmusic->rootPath  =     THINK_PATH; // 设置附件上传根目录
+	    $uploadmusic->savePath  =     '../Public/uploads/'; // 设置附件上传（子）目录
+	    // 上传文件 
+	    $infomusic   =   $uploadmusic->upload();
+
+		$commendModel = D('commend');
+		$data = $commendModel->create();
+
+    	//设置headimg属性值
+    	$data['img'] = $infoimg['img']['savepath'].$infoimg['img']['savename'];
+    	$data['music'] = $infomusic['music']['savepath'].$infomusic['music']['savename'];
+		$commendModel->add($data);
+		$this->redirect("lists");
+
+	}
 }
